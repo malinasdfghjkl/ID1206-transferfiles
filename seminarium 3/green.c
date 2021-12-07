1ce307fa53df61175491c3bd2cab16731da6b6df
malinasdfghjkl <malin.marques@gmail.com>
2021-12-07 13:37:36 +0100

    format

--- seminarium 3/green.c
+++ seminarium 3/green.c
@@ -10,39 +10,36 @@
 #define TRUE 1
 #define STACK_SIZE 4096
 
-
 static ucontext_t main_cntx = {0};
-static green_t main_green = {&main_cntx , NULL, NULL, NULL, NULL, FALSE} ;
+static green_t main_green = {&main_cntx, NULL, NULL, NULL, NULL, FALSE};
 static green_t *running = &main_green;
 
 struct green_t *readyq = NULL;
 static sigset_t block;
 
-static void init() __attribute__ ( (constructor) ) ;
+static void init() __attribute__((constructor));
 
-void init() {
+void init()
+{
     getcontext(&main_cntx);
 }
 
-void enq(green_t **list, green_t *thread){
-    if(*list == NULL)
-    {
-        return;
-    }
-    else
+void enq(green_t **list, green_t *thread)
+{
+    if (*list != NULL)
     {
         green_t *susp = *list;
-        while(susp->next != NULL)
+        while (susp->next != NULL)
         {
             susp = susp->next;
-
         }
         susp->next = thread;
     }
 }
 
-green_t *deq(green_t **list){
-    if(*list == NULL)
+green_t *deq(green_t **list)
+{
+    if (*list == NULL)
     {
         return NULL;
     }
@@ -55,26 +52,30 @@ green_t *deq(green_t **list){
     }
 }
 
-void green_thread(){
-green_t *this = running;
+void green_thread()
+{
+    sigprocmask(SIG_BLOCK, &block, NULL);
 
-void *result = (*this->fun)(this->arg) ;
-// place waiting (joining) thread in ready queue
- if(this->join != NULL){
- enq(&readyq, this->join);
- }
-// save result of execution
-this->retval=result;
+    green_t *this = running;
 
-// we're a zombie
-this->zombie=TRUE;
+    void *result = (*this->fun)(this->arg);
+    // place waiting (joining) thread in ready queue
+    enq(&readyq, this->join);
+    // save result of execution
+    this->retval = result;
 
-// find the next thread to run
-green_t *next= deq(&readyq);
-running = next;
-setcontext(next->context);}
+    // we're a zombie
+    this->zombie = TRUE;
 
-int green_create(green_t *new, void *(*fun)(void*), void *arg)
+    // find the next thread to run
+    green_t *next = deq(&readyq);
+    running = next;
+    setcontext(next->context);
+
+    sigprocmask(SIG_BLOCK, &block, NULL);
+}
+
+int green_create(green_t *new, void *(*fun)(void *), void *arg)
 {
     ucontext_t *cntx = (ucontext_t *)malloc(sizeof(ucontext_t));
     getcontext(cntx);
@@ -101,34 +102,37 @@ int green_create(green_t *new, void *(*fun)(void*), void *arg)
     return 0;
 }
 
-int green_yield( ) {
-green_t * susp = running ;
-// add susp to ready queue
-enq(&readyq, susp);
-
-//select the next thread for execution
-green_t *next= deq(&readyq);
-running = next;
-swapcontext( susp->context , next->context);
-return 0;
-}
-
-int green_join ( green_t *thread , void **res ) {
-if( !thread->zombie ) {
-green_t *susp = running;
-// add as joining thread
-thread->join=susp;
-// select the next thread for execution
-green_t *next = deq(&readyq);
-running = next;
-swapcontext(susp->context, next->context);
-}
-// collect result
-if(thread->retval!=NULL)
+int green_yield()
 {
-*res=thread->retval;
+    green_t *susp = running;
+    // add susp to ready queue
+    enq(&readyq, susp);
+
+    //select the next thread for execution
+    green_t *next = deq(&readyq);
+    running = next;
+    swapcontext(susp->context, next->context);
+    return 0;
 }
-// free context
-free(thread->context);
-return 0;
+
+int green_join(green_t *thread, void **res)
+{
+    if (!thread->zombie)
+    {
+        green_t *susp = running;
+        // add as joining thread
+        thread->join = susp;
+        // select the next thread for execution
+        green_t *next = deq(&readyq);
+        running = next;
+        swapcontext(susp->context, next->context);
+    }
+    // collect result
+    if (thread->retval != NULL)
+    {
+        *res = thread->retval;
+    }
+    // free context
+    free(thread->context);
+    return 0;
 }
