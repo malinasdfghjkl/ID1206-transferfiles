c62b2ff90a031fe1f2c6786f7e1f59813141da5a
malinasdfghjkl <malin.marques@gmail.com>
2021-12-08 11:40:16 +0100

    mutex

--- seminarium 3/green.c
+++ seminarium 3/green.c
@@ -4,12 +4,16 @@
 #include <ucontext.h>
 #include <setjmp.h>
 #include <signal.h>
+#include <sys/time.h>
 #include <assert.h>
 #include "green.h"
 
 #define FALSE 0
 #define TRUE 1
 #define STACK_SIZE 4096
+
+#define PERIOD 100
+
 
 static ucontext_t main_cntx = {0};
 static green_t main_green = {&main_cntx, NULL, NULL, NULL, NULL, FALSE};
@@ -23,6 +27,22 @@ static void init() __attribute__((constructor));
 void init()
 {
     getcontext(&main_cntx);
+
+    //Timer initialization
+    sigemptyset(&block);
+    sigaddset(&block, SIGVTALRM);
+
+    struct sigaction act = {0};
+    struct timeval interval;
+    struct itimerval period;
+
+    act.sa_handler = timer_handler;
+    assert(sigaction(SIGVTALRM, &act, NULL) == FALSE);
+    interval.tv_sec = 0;
+    interval.tv_usec = PERIOD;
+    period.it_interval = interval;
+    period.it_value = interval;
+    setitimer(ITIMER_VIRTUAL, &period, NULL);
 }
 
 void enq(green_t **list, green_t *thread)
@@ -149,12 +169,11 @@ int green_join(green_t *thread, void **res)
 //      # # # # # # # # # #
 
 //Initialize a green condition variable
-void green_cond_init(green_cond_t* cond)
+void green_cond_init(green_cond_t *cond)
 {
     sigprocmask(SIG_BLOCK, &block, NULL);
-    cond->suspthreads=NULL;
+    cond->suspthreads = NULL;
     sigprocmask(SIG_UNBLOCK, &block, NULL);
-
 }
 
 void green_cond_wait(green_cond_t *cond)
@@ -176,12 +195,11 @@ void green_cond_wait(green_cond_t *cond)
     sigprocmask(SIG_UNBLOCK, &block, NULL);
 }
 
-
 void green_cond_signal(green_cond_t *cond)
 {
     sigprocmask(SIG_BLOCK, &block, NULL);
 
-    if(cond->suspthreads == NULL)
+    if (cond->suspthreads == NULL)
     {
         return;
     }
@@ -190,4 +208,82 @@ void green_cond_signal(green_cond_t *cond)
     enq(&readyq, thread);
 
     sigprocmask(SIG_UNBLOCK, &block, NULL);
+}
+
+//      # # # # # # # # # #
+//      # # # # # # # # # #
+//      #  4. Timer # # # #
+//      # # # # # # # # # #
+//      # # # # # # # # # #
+
+void timer_handler(int sig)
+{
+    green_t *susp = running;
+
+    //add the running to the ready queue
+    enq(&readyq, susp);
+    //find the next thread for execution
+    green_t *next = deq(&readyq);
+    running = next;
+    swapcontext(susp->context, next->context);
+}
+
+//      # # # # # # # # # #
+//      # # # # # # # # # #
+//      # #   4. Mutex  # #
+//      # # # # # # # # # #
+//      # # # # # # # # # #
+
+int green_mutex_init(green_mutex_t *mutex)
+{
+    mutex->taken = FALSE;
+    // initialize fields
+    mutex->suspthreads = NULL;
+}
+
+int green_mutex_lock(green_mutex_t *mutex)
+{
+    //block timer interupt
+    sigprocmask(SIG_BLOCK, &block, NULL);
+
+    green_t *susp = running;
+
+    if(mutex->taken)
+    {
+        //suspend the current thread
+        enq(&mutex->suspthreads, susp);
+        //find the next thread
+        green_t *next = deq(&readyq);
+        assert(next != NULL);
+
+        running = next;
+        swapcontext(susp->context, next->context);
+    }
+    else
+    {
+        //take the lock
+        mutex->taken = TRUE;
+    }
+    //unblock
+    sigprocmask(SIG_UNBLOCK, &block, NULL);
+    
+    return 0;
+}
+
+
+int green_mutex_unlock ( green_mutex_t *mutex ) {
+    sigprocmask(SIG_BLOCK, &block, NULL);
+// block timer interrupt
+if( mutex->suspthreads != NULL) {
+// move suspended thread to ready queue
+    green_t *suspthreads = deq(mutex->suspthreads);
+    enq(&readyq, suspthreads);
+} else{
+// release lock aka hard reset
+mutex->taken=FALSE;
+mutex->suspthreads=NULL;
+}
+// unblock
+sigprocmask(SIG_UNBLOCK, &block, NULL);
+return 0;
 }
--- seminarium 3/green.h
+++ seminarium 3/green.h
@@ -15,6 +15,12 @@ typedef struct green_cond_t{
         struct green_t *suspthreads;
 }green_cond_t;
 
+typedef struct green_mutex_t {
+volatile int taken;
+// handle the list
+        struct greeen_t *suspthreads;
+} green_mutex_t;
+
 int green_create(green_t *thread, void *(*fun)(void*), void *arg);
 int green_yield();
 int green_join(green_t *thread, void **val);
@@ -24,3 +30,11 @@ int green_join(green_t *thread, void **val);
 void green_cond_init(green_cond_t* cond);
 void green_cond_wait(green_cond_t *cond);
 void green_cond_signal(green_cond_t *cond);
+
+//timer
+void timer_handler(int);
+
+// Mutex
+int green_mutex_init ( green_mutex_t *mutex );
+int green_mutex_lock ( green_mutex_t *mutex );
+int green_mutex_unlock ( green_mutex_t *mutex );
